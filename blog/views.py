a6812a2a7a7efc0d441db8376c8a6f2d36a2aa21
“Manasvi <manasvi.srivastava@synpulse.com>
2023-11-15 22:59:55 +0100

    second commit

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Manasvi Srivastava',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 14, 2023'
+    },
+    {
+        'author': 'Shreyansh Singh',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 15, 2023'
+    }
+
+]
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
